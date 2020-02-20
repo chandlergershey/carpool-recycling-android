fedc65748e4781602cff4cc46e8e35174057d11f
Neil Mckibben <mckibben.64@osu.edu>
2020-02-20 12:52:55 -0500

    Basic account creation done

--- app/app/src/main/java/com/example/carpool_recycling_app/MainActivity.kt
+++ app/app/src/main/java/com/example/carpool_recycling_app/MainActivity.kt
@@ -3,15 +3,29 @@ package com.example.carpool_recycling_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.carpool_recycling_app.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
+
+val TAG = "debug"
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
+        auth = FirebaseAuth.getInstance() //connects to DB
     }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            Log.d(TAG, "Current user is not signed in")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
--- app/app/src/main/java/com/example/carpool_recycling_app/ui/login/RegistrationActivity.kt
+++ app/app/src/main/java/com/example/carpool_recycling_app/ui/login/RegistrationActivity.kt
@@ -1,13 +1,43 @@
 package com.example.carpool_recycling_app.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.example.carpool_recycling_app.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
 
 class RegistrationActivity : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        val button = findViewById<Button>(R.id.submit)
+        val emailView = findViewById<EditText>(R.id.email)
+        val passwordView = findViewById<EditText>(R.id.password)
+        auth = FirebaseAuth.getInstance()
+//        auth.createUserWithEmailAndPassword()
+        button?.setOnClickListener{
+            createUser(emailView.text.toString(), passwordView.text.toString())
+        }
+    }
+
+    private fun createUser(email: String, password: String) {
+        Log.d("registration", "Email is $email password is $password")
+        auth = FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task: Task<AuthResult> ->
+            if (task.isSuccessful) {
+                //Registration OK
+                val firebaseUser = auth.currentUser!!
+            } else {
+                Log.d("registration", "Sign up failed")
+            }
+        }
     }
 }
