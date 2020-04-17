e698e2e49f13431270d82c625603be0c4a059529
neilmckibben <neilmckibben@gmail.com>
2020-04-16 21:30:36 -0700

    Added nav bar for profile page

--- app/app/src/main/java/com/example/carpool_recycling_app/ProfileActivity.kt
+++ app/app/src/main/java/com/example/carpool_recycling_app/ProfileActivity.kt
@@ -6,25 +6,52 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.carpool_recycling_app.data.model.User
+import com.example.carpool_recycling_app.ui.login.LoginActivity
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.profile.*
 import java.util.*
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var selectPhotoButton: Button
     private lateinit var saveProfileButton: Button
     private lateinit var userNameEditText: EditText
+    private lateinit var auth: FirebaseAuth
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        auth = FirebaseAuth.getInstance() //connects to DB
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        //val selectPhotoButton = findViewById<Button>(R.id.selectphoto_button_register)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onStart() {
@@ -103,6 +130,46 @@ class ProfileActivity : AppCompatActivity() {
             //intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_message -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LatestMessagesActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_create_group -> {
+                Toast.makeText(this, "Groups clicked", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, CreateGroupActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_profile -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_profile -> {
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_map -> {
+                Toast.makeText(this, "Map clicked", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_sign_out -> {
+                Toast.makeText(this, "Sign out clicked", Toast.LENGTH_SHORT).show()
+                auth.signOut()
+                val toast = Toast.makeText(applicationContext, "Signed out", Toast.LENGTH_SHORT)
+                toast.show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
 
